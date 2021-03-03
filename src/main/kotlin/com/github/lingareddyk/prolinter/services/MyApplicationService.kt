e183f61f067e5b73b25a082f502a0a491a3e6c18
GitHub Action <action@github.com>
2021-03-03 03:05:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/lingareddyk/prolinter/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lingareddyk/prolinter/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lingareddyk.prolinter.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lingareddyk.prolinter.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/lingareddyk/prolinter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lingareddyk/prolinter/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lingareddyk.prolinter.services
 
+import com.github.lingareddyk.prolinter.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
